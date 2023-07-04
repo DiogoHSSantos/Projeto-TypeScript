ff0ebe6add56101af03c118dcc996c1b67fd6926
DiogoHSSantos <115512182+DiogoHSSantos@users.noreply.github.com>
2023-07-04 15:36:25 -0300

    Terminei o curso
    Término do Curso de TypeScript parte 1

--- app/app.ts
+++ app/app.ts
@@ -1,4 +1,7 @@
 import { NegociacaoControler } from "./controllers/negociacao-controller.js";//lembre-se sempre de colocar o .js no final para a IDE saber que é um módulo de um arquivo JavaScript.
+import { Negociacao } from "./models/negociacao.js";
+import { Negociacoes } from "./models/negociacoes.js";
+
 
 const controller = new NegociacaoControler();
 
@@ -7,3 +10,4 @@ form.addEventListener('submit', event => {
     event.preventDefault();
     controller.adiciona();
 });
+
--- app/controllers/negociacao-controller.ts
+++ app/controllers/negociacao-controller.ts
@@ -1,9 +1,12 @@
 import { Negociacao } from "../models/negociacao.js";
+import { Negociacoes } from "../models/negociacoes.js";
 
 export class NegociacaoControler {
  private inputData:HTMLInputElement;
  private inputQuantidade:HTMLInputElement;
  private inputValor:HTMLInputElement;
+ private negociacoes = new Negociacoes();
+
 
  constructor(){
     this.inputData = document.querySelector('#data');
@@ -13,7 +16,8 @@ export class NegociacaoControler {
 
  adiciona(): void{
     const negociacao = this.criaNegociacao();
-    console.log(negociacao);
+    this.negociacoes.adiciona(negociacao);
+    console.log(this.negociacoes.lista())
     this.limparFormulario();
  };
 
